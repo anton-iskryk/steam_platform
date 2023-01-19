f5252b510079117cb9be499ffabc86fdb745d088
Anton Iskryk <iskryk.anton@gmail.com>
2023-01-19 18:47:31 +0200

    implement header and game card, place them in the right way on grid

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -3,15 +3,23 @@ import { Header } from '../Header';
 import { GameCard } from '../GameCard';
 // import { Loader } from '../Loader';
 import './HomePage.scss';
+import { TransitionGroup } from 'react-transition-group';
 
 export const HomePage: React.FC = () => {
   return (
     <div className='home-page'>
       <div className='home-page__container'>
+        <>
+          <Header />
 
-        <Header />
-        <GameCard />
-
+          <TransitionGroup className="home-page__all-games">
+              <GameCard />
+              <GameCard />
+              <GameCard />
+              <GameCard />
+              <GameCard />
+          </TransitionGroup>
+        </>
       </div>
     </div>
   );
